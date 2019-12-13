68f6500bab55a43121b2f02f7b7c2530db36ad43
Manuel Valtingojer <braunfuchssysstem@gmail.com>
2019-12-13 12:36:44 +0100

    first commit

---
+++ Swipet/Swipet/Utils/DescriptionGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Swipet.Utils
+{
+    public class DescriptionGenerator
+    {
+        private string[] _adjectives = { "nice", "horrible", "great", "terrible old", "brand new" };
+
+        private string[] _other = { "picture of grandpa", "car", "photo of a forest", "duck" };
+        private static Random random = new Random();
+
+        public string Generate()
+        {
+            var a = _adjectives[random.Next(_adjectives.Count())];
+            var b = _other[random.Next(_other.Count())];
+            return $"A {a} {b}";
+        }
+    }
+}
